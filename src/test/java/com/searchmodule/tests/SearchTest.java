e6620304b62cf65b15cfb67b4c7b7bd162b53d7e
v.muravskyi <v.muravskyi@gmail.com>
2022-06-22 23:58:31 +0300

    commit

--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -1,6 +1,7 @@
 package com.searchmodule.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,7 @@ public class SearchTest extends BaseTest {
 
     @Test
     @Parameters({ "keyword" })
-    public void search(String keyword) {
+    public void search(@Optional String keyword) {
 	SearchPage searchPage = new SearchPage(driver);
 	searchPage.goTo();
 	searchPage.doSearch(keyword);
